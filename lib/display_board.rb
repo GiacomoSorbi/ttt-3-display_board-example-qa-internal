755b1a98823004cea44fbac4d7c2b2a3eccfe046
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-22 14:43:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  (0..2).map{|r| (r>0 ? "-----------\n" : "")+(0..2).map{"   "}.join("|")}.join("\n")
+  puts (0..2).map{|r| (r>0 ? "-----------\n" : "")+(0..2).map{"   "}.join("|")}.join("\n")
 end
