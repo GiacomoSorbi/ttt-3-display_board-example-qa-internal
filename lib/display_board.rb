f6acd3491ebea9dc15ceddc6f4ae10fd8d641e20
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-22 14:43:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts (0..2).map{|r| (r>0 ? "-----------\n" : "")+(0..2).map{"   "}.join("|")}.join("\n")
+end
